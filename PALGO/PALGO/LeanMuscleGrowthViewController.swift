c4f7ae091bbb741ad4a91a2d8a28dea4ec347c41
saqlaingolandaz <saqlaingolandaz@hotmail.com>
2016-03-23 23:33:57 -0400

    Restructured JSON parsing

---
+++ PALGO/PALGO/LeanMuscleGrowthViewController.swift
@@ -0,0 +1,59 @@
+//
+//  LeanMuscleGrowthViewController.swift
+//  PALGO
+//
+//  Created by Saqlain Golandaz on 3/23/16.
+//  Copyright © 2016 SaqlainGolandaz. All rights reserved.
+//
+
+import UIKit
+
+class LeanMuscleGrowthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let requestURL: NSURL = NSURL(string: "http://api.v3.factual.com/t/products-cpg-nutrition?q=whey%20protein%20isolate&select=brand,calories,sodium,potassium&limit=20&KEY=ayw9U0g5JYKAFasWFjeHSEEeBMfTtDoncebQ7gOM")!
+        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(urlRequest) {
+            (data, response, error) -> Void in
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            
+            if (statusCode == 200) {
+                print("Everyone is fine, file downloaded successfully.")
+                do{
+                    
+                    let json : NSDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options:.AllowFragments) as! NSDictionary
+                    
+                    print(json)
+                    
+                }catch {
+                    print("Error with Json: \(error)")
+                }
+            }
+        }
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PALGO/PALGO/ViewController.swift
+++ PALGO/PALGO/ViewController.swift
@@ -10,32 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func leanMuscleGrowth(sender: AnyObject) {
-        let requestURL: NSURL = NSURL(string: "http://api.v3.factual.com/t/products-cpg-nutrition?q=whey%20protein%20isolate&select=brand,calories,sodium,potassium&limit=20&KEY=ayw9U0g5JYKAFasWFjeHSEEeBMfTtDoncebQ7gOM")!
-        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
-        let session = NSURLSession.sharedSession()
-        
-        let task = session.dataTaskWithRequest(urlRequest) {
-            (data, response, error) -> Void in
-            
-            let httpResponse = response as! NSHTTPURLResponse
-            let statusCode = httpResponse.statusCode
-            
-            if (statusCode == 200) {
-                print("Everyone is fine, file downloaded successfully.")
-                do{
-                    
-                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options:.AllowFragments)
-                    
-                    print(json)
-                    
-                }catch {
-                    print("Error with Json: \(error)")
-                }
-            }
-        }
-    task.resume()
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
