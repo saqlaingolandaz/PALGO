2648787624f2e6b934d1a202529c05cce6460da7
saqlaingolandaz <saqlaingolandaz@hotmail.com>
2016-06-06 13:44:12 -0400

    Added txt file

---
+++ PALGO/PALGO/GeneralWorkoutViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GeneralWorkoutViewController.swift
+//  PALGO
+//
+//  Created by Saqlain Golandaz on 6/6/16.
+//  Copyright © 2016 SaqlainGolandaz. All rights reserved.
+//
+
+import UIKit
+
+class GeneralWorkoutViewController: UIViewController {
+    
+    var contents : NSString = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        if let filepath = NSBundle.mainBundle().pathForResource("test", ofType: "txt") {
+//            do {
+//                let contents = try NSString(contentsOfFile: filepath, usedEncoding: nil) as String
+//                print(contents)
+//            } catch {
+//                print("doesn't work")
+//                // contents could not be loaded
+//            }
+//        } else {
+//            // example.txt not found!
+//        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
+
