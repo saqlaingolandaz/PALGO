8c988e00c96b6539232e3f42ec171ed9a6d7d537
saqlaingolandaz <saqlaingolandaz@hotmail.com>
2016-06-08 14:25:29 -0400

    Added more data to text file, converted string to float, created third question, programmed segues

--- PALGO/PALGO/GeneralWorkoutViewController.swift
+++ PALGO/PALGO/GeneralWorkoutViewController.swift
@@ -13,7 +13,12 @@ class GeneralWorkoutViewController: UIViewController {
     var contents : NSString = "";
     var masterArray : [[String]] = [];
     var names : [String] = [];
-    var costs : [String] = [];
+    var costs : [Float] = [];
+    
+    var cheap : [Float] = [];
+    var medium : [Float] = [];
+    var expensive : [Float] = [];
+    
     
     override func viewDidLoad() {
         if let filepath = NSBundle.mainBundle().pathForResource("Untitled", ofType: "txt") {
@@ -32,11 +37,12 @@ class GeneralWorkoutViewController: UIViewController {
         for(var i = 0; i < lines.count; i++) {
             masterArray.append(lines[i].componentsSeparatedByString(",") as Array)
             names.append(masterArray[i][0])
-            costs.append(masterArray[i][1])
+            costs.append(Float(masterArray[i][1])!)
         }
-        print(masterArray)
-        print(names)
+        //print(masterArray)
+        //print(names)
         print(costs)
+        print(costs.indexOf(21.99))
 
         
 //        var price : Float = Float(lines2[1])!
@@ -49,6 +55,48 @@ class GeneralWorkoutViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        if (segue.identifier == "one") {
+            for(var i = 0; i < costs.count; i++) {
+                if(costs[i] <= 39.99) {
+                    var temp = Float(costs.indexOf(costs[i])!)
+                    cheap.append(temp)
+                }
+            }
+        } else if (segue.identifier == "two") {
+            for(var i = 0; i < costs.count; i++) {
+                if(costs[i] > 40.0 && costs[i] < 60.0) {
+                    var temp = Float(costs.indexOf(costs[i])!)
+                    medium.append(temp)
+                }
+            }
+        } else if (segue.identifier == "three") {
+            for(var i = 0; i < costs.count; i++) {
+                if(costs[i] > 60.0) {
+                    var temp = Float(costs.indexOf(costs[i])!)
+                    expensive.append(temp)
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func oneClicked(sender: AnyObject) {
+        print(cheap)
+        performSegueWithIdentifier("one", sender: self)
+    }
+
+    @IBAction func twoClicked(sender: AnyObject) {
+        print(medium)
+        performSegueWithIdentifier("two", sender: self)
+    }
+    
+    
+    @IBAction func threeClicked(sender: AnyObject) {
+        print(expensive)
+        performSegueWithIdentifier("three", sender: self)
     }
     
     
